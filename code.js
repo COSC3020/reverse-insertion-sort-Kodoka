4ce94185dc6b617aa3c910866107e60902a7b2fb
Kodoka <10721764+Kodoka@users.noreply.github.com>
2025-03-21 13:46:12 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,10 +1,10 @@
 function insertionSortReverse(array)
 {
-    for(var i = array.length; i > 1; i--) // Start at end of array.
+    for(var i = array.length - 1; i > 1; i--) // Start at end of array.
     {
         var val = array[i]; // Assign last element of unsorted to val
         var j;
-        for(var j = array.length; j > 0 && array[j-1] > val; j--)
+        for(var j = array.length - 1; j > 0 && array[j-1] > val; j--)
         {
             array[j] = array[j-1];
         }
