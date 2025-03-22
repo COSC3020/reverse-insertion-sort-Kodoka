0bb5d3f9f5e20b2f437fbbccb3d87df2892f2120
Kodoka <10721764+Kodoka@users.noreply.github.com>
2025-03-21 18:10:55 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,13 +1,17 @@
 function insertionSortReverse(array)
 {
-    for(var i = array.length - 1; i > 1; i--) // Start at end of array.
-    {
-        var val = array[i]; // Assign last element of unsorted to val
-        var j;
-        for(var j = array.length - 1; j > 0 && array[j-1] > val; j--)
+    // Start at second to last elemtnt of array, move towards front. Last element should be trivially sorted.
+    for(var i = array.length - 2; i >= 0; i--) {
+        // Assign last element of unsorted portion of array to val.
+        var val = array[i];
+        // Compare the element, val, to the next element. If the element to the right is lesser, shift it left. Also
+        // checks we aren't at end of array.
+        for (var j = i; j < array.length - 1 && array[j + 1] < val; j++)
         {
-            array[j] = array[j-1];
+            // Shift element from right to left.
+            array[j] = array[j + 1];
         }
+        // Shift val left.
         array[j] = val;
     }
     return array;
